87c80b6d7b4397ae161f21c7e6ab859de03231fc
Ezequiel Merino <vmware@ditec.es>
2025-12-02 16:43:57 +0100

    now works moving the square

--- src/main.c
+++ src/main.c
@@ -6,19 +6,19 @@ int main() {
     // Control de entrada de teclado
     install_keyboard();
     // Crear una pantalla de 640x480
-    set_color_depth(32);
-    set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
+    set_color_depth(16);
+    set_gfx_mode(GFX_AUTODETECT_FULLSCREEN, 640,480, 0, 0);
 
     // Buffer para dibujar
-    BITMAP *buffer = create_bitmap(640, 480);
+    BITMAP *buffer = create_bitmap(640,480);
 
     // iniciarilzar coordenadas de un rectángulo
     int x1,x2,y1,y2;
-    x1=0; x2=0; y1=20; y2=20;
+    x1=0; x2=20; y1=0; y2=20;
 
     // BUCLE PRINCIPAL
     while (!key[KEY_ESC]) {
-        // Limpiar el buffer
+       // Limpiar el buffer
         clear_to_color(buffer, makecol(0, 0, 0));
 
         // Dibujar un rectángulo relleno con RGB
@@ -30,13 +30,13 @@ int main() {
         // Actualizar las coordenadas
         //x1 += 2; x2 += 2;
         //if (x1 > 640) { x1 = 0; x2 = 0; }
-        if (key[KEY_RIGHT]) { x1 += 2; x2 += 2; }
-        if (key[KEY_LEFT])  { x1 -= 2; x2 -= 2; }
+        if (key[KEY_RIGHT]) { x1 += 2; x2 += 2;}
+        if (key[KEY_LEFT])  { x1 -= 2; x2 -= 2;}
         if (x1 < 0) { x1 = 0; x2 = 0; }
         if (x2 > 640) { x2 = 640; x1 = 640 - (x2 - x1); }
 
-        if (key[KEY_DOWN]) { y1 += 2; y2 += 2; }
-        if (key[KEY_UP])   { y1 -= 2; y2 -= 2; }
+        if (key[KEY_DOWN]) { y1 += 2; y2 += 2;}
+        if (key[KEY_UP])   { y1 -= 2; y2 -= 2;}
         if (y1 < 0) { y1 = 0; y2 = 0; }
         if (y2 > 480) { y2 = 480; y1 = 480 - (y2 - y1); }
         
@@ -44,11 +44,10 @@ int main() {
         // Copiar el buffer a la pantalla
         blit(buffer, screen, 0, 0, 0, 0, 640, 480);
         //bitmap, destino, sx, sy, dx, dy, w, h)
-
+        
         // Esperar un poco
         rest(20);
     }
-    rectfill(buffer, 100, 100, 200, 200, makecol
 
     return 0;
 }
